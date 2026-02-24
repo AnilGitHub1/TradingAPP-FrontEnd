a97d2d8c72b18515b2243aaeb303c498725ac772
AnilGitHub1 <63957208+AnilGitHub1@users.noreply.github.com>
2026-02-24 14:01:35 +0530

    Fix stocks page grid classes so chart and toolbar span correctly

--- src/Routes/Stocks.jsx
+++ src/Routes/Stocks.jsx
@@ -12,22 +12,22 @@ export default function Stocks() {
   return (
     <div className="stocks-page container-fluid">
       {searchActive && <Screen />}
-
       {searchActive && <Search />}
 
       <div style={searchActive ? { filter: "blur(2px)" } : {}}>
         <div className="row g-0">
-          <div className="col-12">
+          <div className="col">
             <StockBar />
           </div>
         </div>
 
         <div className="row g-0 stocks-content-row">
-          <div className="col-lg-9 col-xl-10 chart-panel">
+          <div className="col-10 chart-panel">
             <ChartComponent />
             {tradeBoxActive && <TradeBox />}
           </div>
-          <div className="col-lg-3 col-xl-2 stocklist-panel">
+
+          <div className="col-2 stocklist-panel">
             <StockList />
           </div>
         </div>
